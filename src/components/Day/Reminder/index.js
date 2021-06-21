1d5cde78dcb6bf2c5aca74966fe9c8fc3d766c84
romie.nugraha <romie.nugraha@indivaragroup.com>
2021-06-21 10:15:33 +0700

    initial init

---
+++ src/components/Day/Reminder/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+
+const reminder = props => (
+  <article className="reminder" style={{ background: props.reminder.color }}>
+    <div className="tools">
+      <button onClick={() => props.handleDeleteReminder(props.reminder.id)}>
+        <i className="fas fa-trash-alt" />
+      </button>
+      <button onClick={() => props.handleSetEdit(props.reminder)}>
+        <i className="fas fa-edit" />
+      </button>
+    </div>
+    <strong>{props.reminder.description}</strong>
+    <p>{props.reminder.email}</p>
+    <time>{props.reminder.time}</time>
+  </article>
+);
+
+export default reminder;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./styles/main.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+// Reducers
+import reminders from "./store/reducers/reminders";
+
+const store = createStore(reminders, applyMiddleware(thunk));
+
+const appShell = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(appShell, document.getElementById("root"));
+registerServiceWorker();
